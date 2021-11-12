d68229c1735986bf6acc3a41b26c3c10785567ff
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-11-12 02:07:27 +0100

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as http from "http";
+import express from "express";
+import WebSocket from "ws";
+import { Message, MessageModule, MessageRequest } from "./types";
+
+const port = 4000;
+
+const app = express();
+
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const webSocketServer = new WebSocket.Server({ server });
+
+const send = (msg: MessageRequest, module: MessageModule) => {
+  const message: Message = {
+    type: msg.type,
+    module: module,
+    payload: msg.payload,
+  };
+  webSocketServer.clients.forEach((client) => {
+    client.send(JSON.stringify(message));
+  });
+};
+
+app.post("/controller", (req: express.Request, res: express.Response) => {
+  send(req.body, MessageModule.BAKSET);
+  res.send({ result: "ok" });
+});
+
+server.listen(port, () => {
+  console.log(`bff running at ${port}`);
+});
